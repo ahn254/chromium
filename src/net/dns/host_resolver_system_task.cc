28684db321e97aa3d0deda129689c1d3f3268900
klzgrad <kizdiv@gmail.com>
2023-03-05 09:25:27 +0800

    win: Fix headers

--- src/net/dns/host_resolver_system_task.cc
+++ src/net/dns/host_resolver_system_task.cc
@@ -13,6 +13,7 @@
 #include "base/no_destructor.h"
 #include "base/sequence_checker.h"
 #include "base/sequence_checker_impl.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
